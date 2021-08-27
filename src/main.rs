943bedfcc03351b45567ecbfa52684ad957fa40f
Jacob See <jasee@jasee-mac.lan>
2021-08-26 23:31:12 -0800

    Make main.rs cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 mod components;
 
-pub use crate::components::AndGate;
+pub use crate::components::{AndGate, wiring};
 
 fn main() {
     let mut and_gate = AndGate::default();
     and_gate.settle();
-    println!("{}", and_gate.output.lock().unwrap());
-    {
-        let mut input1_lock = and_gate.input1.lock().unwrap();
-        *input1_lock = true;
-    }
+    println!("Inputs: {}, {} Output: {}", and_gate.input1.lock().unwrap(), and_gate.input2.lock().unwrap(), and_gate.output.lock().unwrap());
+
+    wiring::set_high(&mut and_gate.input1);
     and_gate.settle();
-    println!("{}", and_gate.output.lock().unwrap());
-    {
-        let mut input2_lock = and_gate.input2.lock().unwrap();
-        *input2_lock = true;
-    }
+    println!("Inputs: {}, {} Output: {}", and_gate.input1.lock().unwrap(), and_gate.input2.lock().unwrap(), and_gate.output.lock().unwrap());
+
+    wiring::set_high(&mut and_gate.input2);
     and_gate.settle();
-    println!("{}", and_gate.output.lock().unwrap());
+    println!("Inputs: {}, {} Output: {}", and_gate.input1.lock().unwrap(), and_gate.input2.lock().unwrap(), and_gate.output.lock().unwrap());
 }
